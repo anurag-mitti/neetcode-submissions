5eb7ca8232229070b3c11f7032c458ec38537c95
Anurag Mitti <anuragmitti@gmail.com>
2026-06-22 16:26:03 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    bool check(string s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+
+        while (l < r) {
+            if (s[l] == s[r]) {
+                l++;
+                r--;
+            } else {
+                return check(s, l + 1, r) ||
+                       check(s, l, r - 1);
+            }
+        }
+
+        return true;
+    }
+};
